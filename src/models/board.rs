6dba6b75880efaae22874e22637b980938201dc5
devtrium <devtrium325@gmail.com>
2018-04-10 22:46:04 +0800

    use snake case

--- src/models/board.rs
+++ src/models/board.rs
@@ -3,12 +3,12 @@ use models::tile::Tile;
 pub struct Board {
     tiles: Vec<Tile>,
     size: u32,
-    maxHeight: u32,
-    maxWidth: u32
+    max_height: u32,
+    max_width: u32
 }
 
 impl Board {
-    pub fn new (size: u32, maxWidth: u32, maxHeight: u32) -> Board {
+    pub fn new (size: u32, max_width: u32, max_height: u32) -> Board {
         let mut list_tiles = Vec::new();
 
         // Set the value content (x,y) axis
@@ -32,22 +32,22 @@ impl Board {
 
                 // get the y axis for next content
                 y = y + list_tiles[(j-1) as usize].height + 5;
-                if y >= maxHeight - list_tiles[(j-1) as usize].height {
+                if y >= max_height - list_tiles[(j-1) as usize].height {
                     break;
                 }
             }
             //get the x axis for next content
             x = x + list_tiles[0].width + 5;
-            if x >= maxWidth - list_tiles[0].width {
+            if x >= max_width - list_tiles[0].width {
                 break;
             }
         }
 
         Board {
             tiles: list_tiles,
-            size: size,
-            maxHeight: maxHeight,
-            maxWidth: maxWidth
+            size,
+            max_height,
+            max_width
         }
     }
 
