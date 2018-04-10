d9d0cc9a496416c7987d5f6f1a34857e7781e4b4
devtrium <devtrium325@gmail.com>
2018-04-10 21:47:50 +0800

    create board and tiles

--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,23 @@ pub mod models;
 
 #[no_mangle]
 pub fn add(a: i32, b: i32) -> i32 {
-    return 14
+    return 2020
 }
 
 #[cfg(test)]
 mod tests {
-    use super::models::{Tile};
+    use super::models::{Tile, Board};
 
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
         let t = Tile::new();
         assert_eq!(t.get_size(), 10);
+
+        let w = Board::new(3);
+        assert_eq!(w.get_size(), 9);
+
+        let w = Board::new(10);
+        assert_eq!(w.get_size(), 100);
     }
 }
--- src/models/board.rs
+++ src/models/board.rs
@@ -7,9 +7,20 @@ pub struct Board {
 
 impl Board {
     pub fn new (size: u32) -> Board {
+        let mut list_tiles = Vec::new();
+        for i in 0..size {
+            for j in 0..size {
+                list_tiles.push(Tile::new())
+            }
+        }
+
         Board {
-            tiles: vec![],
+            tiles: list_tiles,
             size: size
         }
+    }
+
+    pub fn get_size(&self) -> usize {
+        self.tiles.len()
     }
 }
