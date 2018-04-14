5f8b97048f583429db7e0ff6059f86ec3cb1c955
devtrium <devtrium325@gmail.com>
2018-04-14 16:53:10 +0800

    update game play

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import { next_move } from "./tic_to_wasm";
 
 // DOM manipulation will be handle by js
-
 // create grid and event handler based on collision detection
 // inspired by http://jsfiddle.net/BmeKr/1800/
 
@@ -10,7 +9,7 @@ var elem = document.getElementById('myCanvas'),
   elemTop = elem.offsetTop,
   context = elem.getContext('2d'),
   elements = [],
-  size = 3
+  size = 5
 
 // Set board size
 elem.width = elem.height = size * 80 + size *  5 + 2 * 20
@@ -21,8 +20,18 @@ elem.addEventListener('click', function (event) {
     y = event.pageY - elemTop;
 
   elements.forEach(function (element) {
-    if (y > element.top && y < element.top + element.height && x > element.left && x < element.left + element.width) {
-    alert(element.text);
+    if (y > element.top && y < element.top + element.height && 
+      x > element.left && x < element.left + element.width) {
+      if (element.belongsTo === 'neutral') {
+        element.belongsTo = 'human'
+        element.text = '0'
+
+        let robot = elements[next_move(get_condition(elements))];
+        robot.text = 'X'
+        robot.belongsTo = 'robot'
+
+        elements.forEach(render_tile);
+      }
     }
   });
 }, false);
@@ -38,11 +47,6 @@ var x = 20, y = 20, maxWidth = elem.getAttribute('width'),
 for (var i = 1; i <= size; i++) {
   y = 20;
   for (var j = 1; j <= size; j++) {
-    if (i === j) {
-      text = 'X'
-    } else {
-      text = 'O'
-    }
 
     elements.push({
       colour: '#05EFFF',
@@ -50,8 +54,9 @@ for (var i = 1; i <= size; i++) {
       height: height,
       top: y,
       left: x,
-      text: text,
+      text: '',
       textColour: '#fff',
+      belongsTo: 'neutral'
     });
 
     // get the y axis for next content
@@ -78,5 +83,20 @@ function render_tile(element) {
 
 // Render elements.
 elements.forEach(render_tile);
-console.log(`next_move => ${next_move("1,0,0,0,0,0,0,0,0")}`);
-console.log(`next_move => ${next_move("1,2,0,1,1,0,0,0,0")}`);
+//console.log(`next_move => ${next_move("1,0,0,0,0,0,0,0,0")}`);
+//console.log(`next_move => ${next_move("1,2,0,1,1,0,0,0,0")}`);
+function get_condition(elements) {
+  var results = []
+
+  elements.forEach(function(e,i) {
+    if (e.belongsTo === 'human') {
+      results.push(1)
+    } else if (e.belongsTo === 'robot') {
+      results.push(2)
+    } else {
+      results.push(0)
+    }
+  });
+
+  return results.join(',')
+}
